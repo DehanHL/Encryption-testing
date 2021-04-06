3d95809de9ce97cc19cb8879d8c6ed0451a1e10d
DEHAN LENTING <34535802@student365.msfed.nwu.ac.za>
2021-04-06 19:35:48 +0200

    Encryption test

---
+++ Encryption testing/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace Encryption_testing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnc = new System.Windows.Forms.Button();
+            this.rtbOut = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblHash = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblPt = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rtbOutDe = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.OpenFD = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEnc
+            // 
+            this.btnEnc.Location = new System.Drawing.Point(61, 102);
+            this.btnEnc.Name = "btnEnc";
+            this.btnEnc.Size = new System.Drawing.Size(188, 40);
+            this.btnEnc.TabIndex = 0;
+            this.btnEnc.Text = "Custom cipher encrypt";
+            this.btnEnc.UseVisualStyleBackColor = true;
+            this.btnEnc.Click += new System.EventHandler(this.btnEnc_Click);
+            // 
+            // rtbOut
+            // 
+            this.rtbOut.Location = new System.Drawing.Point(12, 181);
+            this.rtbOut.Name = "rtbOut";
+            this.rtbOut.Size = new System.Drawing.Size(311, 96);
+            this.rtbOut.TabIndex = 1;
+            this.rtbOut.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Current constant password: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2020BEGONE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hash:";
+            // 
+            // lblHash
+            // 
+            this.lblHash.AutoSize = true;
+            this.lblHash.Location = new System.Drawing.Point(56, 39);
+            this.lblHash.Name = "lblHash";
+            this.lblHash.Size = new System.Drawing.Size(420, 15);
+            this.lblHash.TabIndex = 5;
+            this.lblHash.Text = "E076143A6A0533278CA39EA03C37182ACAD87E0AB0E0B2C397999519D38244BF";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Current constant plaintext:";
+            // 
+            // lblPt
+            // 
+            this.lblPt.AutoSize = true;
+            this.lblPt.Location = new System.Drawing.Point(165, 65);
+            this.lblPt.Name = "lblPt";
+            this.lblPt.Size = new System.Drawing.Size(79, 15);
+            this.lblPt.TabIndex = 7;
+            this.lblPt.Text = "bytestringtest";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Result";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(400, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Result";
+            // 
+            // rtbOutDe
+            // 
+            this.rtbOutDe.Location = new System.Drawing.Point(399, 181);
+            this.rtbOutDe.Name = "rtbOutDe";
+            this.rtbOutDe.Size = new System.Drawing.Size(311, 96);
+            this.rtbOutDe.TabIndex = 9;
+            this.rtbOutDe.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(400, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 40);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Custom cipher de-encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Choose File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(94, 330);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(38, 15);
+            this.lblFileName.TabIndex = 13;
+            this.lblFileName.Text = "label7";
+            // 
+            // OpenFD
+            // 
+            this.OpenFD.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 326);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(338, 23);
+            this.textBox1.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(165, 305);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Byte string:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.rtbOutDe);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblPt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblHash);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbOut);
+            this.Controls.Add(this.btnEnc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnc;
+        private System.Windows.Forms.RichTextBox rtbOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblHash;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblPt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox rtbOutDe;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog OpenFD;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Encryption testing/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encryption_testing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string Hash;
+        string pText;
+        string cipherResult = "";
+        string selectedFileName;
+        private void btnEnc_Click(object sender, EventArgs e)
+        {
+            Hash = lblHash.Text;
+            pText = lblPt.Text;
+
+            int num;
+            int icHash = 0;
+
+            for (int i = 0; i < (pText.Length); i++)
+            {
+                num = Convert.ToInt32(pText[i]) + Convert.ToInt32(Hash[icHash]);
+
+                cipherResult = cipherResult + Convert.ToChar(num);
+
+                //range would be set to however large the hash is
+                if (icHash >= Hash.Length)
+                {
+                    icHash = 0;
+                }
+                else
+                {
+                    icHash += 1;
+                }
+            }
+
+            rtbOut.Text = cipherResult;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num;
+            int icHash = 0;
+            string res = "";
+            for (int i = 0; i < (cipherResult.Length); i++)
+            {
+                num = Convert.ToInt32(cipherResult[i]) - Convert.ToInt32(Hash[icHash]);
+
+                res = res + Convert.ToChar(num);
+
+                if (icHash >= Hash.Length)
+                {
+                    icHash = 0;
+                }
+                else
+                {
+                    icHash += 1;
+                }
+            }
+
+            rtbOutDe.Text = res;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "e:\\";
+            openFileDialog1.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                selectedFileName = openFileDialog1.FileName;
+                lblFileName.Text = selectedFileName;
+            }
+
+
+        }
+    }
+}
